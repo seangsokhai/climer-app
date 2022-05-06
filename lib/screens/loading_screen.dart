85a2c0379fb053601a5bc25bf346d43a0471dbe1
Sokhai Seang <saihok71@gmail.com>
2022-05-06 18:15:10 +0700

    climer app

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: avoid_print
+
+import 'package:climer/services/location.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get location"),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
